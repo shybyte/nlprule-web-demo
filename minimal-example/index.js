e04d6189f6db8b0e4d3775023ebd4ab45c73a1a0
shybyte <shybyte@users.noreply.github.com>
2021-06-19 01:17:24 +0200

    Make it Interactive

--- minimal-example/index.js
+++ minimal-example/index.js
@@ -2,6 +2,21 @@ import * as wasm from "nlprule-wasm";
 
 const nlpRuleChecker = wasm.NlpRuleChecker.new();
 
-console.log("suggestions: " ,nlpRuleChecker.check('She was not been here since Monday.'))
+console.log("suggestions2: " ,nlpRuleChecker.check('She was not been here since Monday.'))
 
 window.nlpRuleChecker = nlpRuleChecker;
+
+const textInputField = document.getElementById('textInputField');
+const correctionsField = document.getElementById('correctionsField');
+
+function checkTextInput() {
+  const corrections = nlpRuleChecker.check(textInputField.value);
+  correctionsField.value = corrections.length > 0
+    ? JSON.stringify(corrections, null, 2)
+    : 'I have found no issue.'
+
+}
+
+textInputField.addEventListener('input', checkTextInput);
+
+checkTextInput();
