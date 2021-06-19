806efa278669537ee3604979d9566e76668ffbab
shybyte <shybyte@users.noreply.github.com>
2021-06-19 14:48:11 +0200

    Add loading spinner

--- minimal-example/index.js
+++ minimal-example/index.js
@@ -2,21 +2,41 @@ import * as wasm from "nlprule-wasm";
 
 const nlpRuleChecker = wasm.NlpRuleChecker.new();
 
-console.log("suggestions2: " ,nlpRuleChecker.check('She was not been here since Monday.'))
+window.nlpRuleChecker = nlpRuleChecker; // Export for debugging
 
-window.nlpRuleChecker = nlpRuleChecker;
-
+const textInputForm = document.getElementById('textInputForm');
 const textInputField = document.getElementById('textInputField');
 const correctionsField = document.getElementById('correctionsField');
+const loadingSpinner = document.getElementById('loadingSpinner');
 
 function checkTextInput() {
   const corrections = nlpRuleChecker.check(textInputField.value);
   correctionsField.value = corrections.length > 0
     ? JSON.stringify(corrections, null, 2)
     : 'I have found no issue.'
-
 }
 
-textInputField.addEventListener('input', checkTextInput);
+async function checkTextInputWithLoadingSpinner() {
+  loadingSpinner.style.display = 'block';
+  await sleep(1); // Allow the UI thread to show the spinner.
+  console.log('Start Check');
+  console.time('Check');
+  checkTextInput();
+  console.timeEnd('Check');
+  loadingSpinner.style.display = 'none';
+}
 
-checkTextInput();
+textInputForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  checkTextInputWithLoadingSpinner();
+})
+
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    });
+  }, ms);
+}
+
+checkTextInputWithLoadingSpinner();
