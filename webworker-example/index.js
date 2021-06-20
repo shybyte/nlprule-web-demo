8fcaee815f92ef6dfada749360476089ff38d12f
shybyte <shybyte@users.noreply.github.com>
2021-06-20 22:58:01 +0200

    webworker-example

---
+++ webworker-example/index.js
@@ -0,0 +1,34 @@
+const nlpruleWorker = new Worker(new URL('./nlprule-webworker.js', import.meta.url));
+
+const textInputForm = document.getElementById('textInputForm');
+const textInputField = document.getElementById('textInputField');
+const checkButton = document.getElementById('checkButton');
+const correctionsField = document.getElementById('correctionsField');
+const loadingSpinner = document.getElementById('loadingSpinner');
+
+function checkTextInput() {
+  console.log('Start Check');
+  checkButton.disabled = true;
+  loadingSpinner.style.display = 'block';
+  nlpruleWorker.postMessage({text: textInputField.value});
+}
+
+nlpruleWorker.onmessage = ({data: {eventType, corrections}}) => {
+  switch (eventType) {
+    case 'loaded':
+      checkTextInput();
+      return
+    case 'checkFinished':
+      loadingSpinner.style.display = 'none';
+      checkButton.disabled = false;
+      correctionsField.value = corrections.length > 0
+        ? JSON.stringify(corrections, null, 2)
+        : 'I have found no issue.'
+  }
+};
+
+textInputForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  checkTextInput();
+})
+
